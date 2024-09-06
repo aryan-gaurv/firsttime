ed15492ece0b057a15598df8b5f4b82f5327ef34
Aryan Gaurav <garyan497@gmail.com>
2024-09-06 14:05:14 +0530

    hi

---
+++ add.java
@@ -0,0 +1,16 @@
+public class add {
+
+    public static void main(String[] args) {
+        int a[]={3,1,8,5,4,9,2};
+        int n=9;
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=i+1;j<a.length;j++)
+        {
+            if((a[i]+a[j])==n)
+            System.out.println(i+","+j);
+        }
+        }
+System.out.println("Hello")
+    }
+}
